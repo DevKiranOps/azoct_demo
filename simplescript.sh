4a408781b9fa680e13275a057b457d81ce3a28a5
kiran <kiranopskumar@gmail.com>
2020-11-18 07:58:02 +0530

    defined role var and printed and validated

--- simplescript.sh
+++ simplescript.sh
@@ -2,8 +2,13 @@
 
 # This is a demo script to demonstrate version control tracking
 
-
+source ./vars.txt
 echo "This is test"
 echo "Garbage Right now"
 
 ehco "adklfjawdlj"
+
+if [[ $role == "devops"]]
+then
+     echo "Right fit"
+fi 
