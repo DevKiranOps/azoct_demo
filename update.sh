88babafd4d8e03ae2d765fbb184f27011c041bd9
kiran <kiranopskumar@gmail.com>
2020-11-18 08:08:03 +0530

    added new statements to update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 source ./vars.txt
 echo "Test Test Test"
 echo "the name is $name"
+ehco "your name is blabalabd;alhdf"
 echo "$name's ID is $id"
 echo "Age of $name is $age "
+echo "topper of the batch is $name"
 echo "user's role is $role"
