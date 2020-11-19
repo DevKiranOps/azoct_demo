56d7d280c35b6b9eee192dc530a0f7da73554ab3
kiran <kiranopskumar@gmail.com>
2020-11-19 07:47:33 +0530

    remvoed father's name var

--- simplescript.sh
+++ simplescript.sh
@@ -2,13 +2,8 @@
 
 # This is a demo script to demonstrate version control tracking
 
-source ./vars.txt
+
 echo "This is test"
 echo "Garbage Right now"
 
 ehco "adklfjawdlj"
-
-if [[ $role == "devops"]]
-then
-     echo "Right fit"
-fi 
