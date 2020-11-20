86aa25f7173247d7c416fc72a4047c3efa3816d1
Kiran <kiranopskumar@gmail.com>
2020-11-20 08:21:37 +0530

    checking if vars.txt is present or not

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 source ./vars.txt
-echo "Test Test Test"
+echo "We are about to start"
+if [[ -f vars.txt ]]
+then
+	source ./vars.txt
+else
+	echo "Vars file missing"
+fi
 echo "the name is $name"
 echo "$name's ID is $id"
 echo "Age of $name is $age "
